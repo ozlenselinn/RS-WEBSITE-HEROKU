77e33295e997e676f84a802b5882fb0dc0c32bc2
ozlenselinn <ozlen_selin@hotmail.com>
2022-02-19 23:26:01 +0300

    blog sayfası -not completed-

--- src/Components/Pages/Blog/Blog.js
+++ src/Components/Pages/Blog/Blog.js
@@ -1,167 +1,239 @@
 import React from 'react';
-import { styled } from '@mui/material/styles';
-// import Box from '@mui/material/Box';
-// import Paper from '@mui/material/Paper';
-// import Grid from '@mui/material/Grid';
-// import niyet_mektubu from './niyet_mektubu.jpg';
-// import motivasyon from './motivasyo.jpg';
-// import niyet from './niyet.jpg';
-// import is from './is.jpg';
-// import cv from './cv.png';
-// import Style from './BlogStyle.css';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Avatar from '@material-ui/core/Avatar';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import Pagination from '@material-ui/lab/Pagination';
 
-// import { LinkBox, LinkOverlay } from '@chakra-ui/react';
-// import { Heading } from '@chakra-ui/react';
-// import { Text } from '@chakra-ui/react';
-
-
-import Box from '@mui/material/Box';
-import Chip from '@mui/material/Chip';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Divider from '@mui/material/Divider';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-
-const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    backgroundColor: "#fff"
+  },
+  hero: {
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://images.unsplash.com/photo-1558981852-426c6c22a060?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')`,
+    height: "500px",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#fff",
+    fontSize: "4rem",
+    [theme.breakpoints.down("sm")]: {
+      height: 300,
+      fontSize: "3em"
+    }
+  },
+  blogsContainer: {
+    paddingTop: theme.spacing(3)
+  },
+  blogTitle: {
+    fontWeight: 800,
+    paddingBottom: theme.spacing(3)
+  },
+  card: {
+    maxWidth: "100%",
+  },
+  media: {
+    height: 240
+  },
+  cardActions: {
+    display: "flex",
+    margin: "0 10px",
+    justifyContent: "space-between"
+  },
+  author: {
+    display: "flex"
+  },
+  paginationContainer: {
+    display: "flex",
+    justifyContent: "center"
+  }
 }));
-const Img = styled('img')({
-       margin: 'auto',
-       display: 'block',
-       width:"500px",
-       maxWidth: '100%',
-       maxHeight: '100%',
-       paddingBottom:'5%',
-     });
-const Blog = () => {
-       return (
-              <>              
-      <Box sx={{ width: '100%', maxWidth: 1250, bgcolor:"background.paper"}}>
-      <Box sx={{ mx:2 }}>
-        <Grid container alignItems="center">
-          <Grid item xs>
-            <Typography gutterBottom variant="h5" component="div">
-              Doktora Başvuruları: Kapsamlı Rehber
-            </Typography>
+
+function Blog() {
+  const classes = useStyles();
+
+  return (
+    <div className="App">
+      <AppBar className={classes.appBar} position="static">
+        <Toolbar>
+          <Typography variant="h6" color="primary" >
+            Blog
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box className={classes.hero}>
+        <Box>React Blog</Box>
+      </Box>
+      <Container maxWidth="lg" className={classes.blogsContainer}>
+        <Typography variant="h4" className={classes.blogTitle}>
+          Articles
+        </Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="https://images.pexels.com/photos/2004161/pexels-photo-2004161.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    React useContext
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                    across all continents except Antarctica
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions className={classes.cardActions}>
+                <Box className={classes.author}>
+                  <Avatar src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
+                  <Box ml={2}>
+                    <Typography variant="subtitle2" component="p">
+                      Guy Clemons
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary" component="p">
+                      May 14, 2020
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box>
+                  <BookmarkBorderIcon />
+                </Box>
+              </CardActions>
+            </Card>
           </Grid>
-          <Grid item>
-            <Typography gutterBottom variant="h6" component="div">
-             16 Nisan 2021
-            </Typography>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="https://images.pexels.com/photos/34600/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    React Router
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                    across all continents except Antarctica
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions className={classes.cardActions}>
+                <Box className={classes.author}>
+                  <Avatar src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2734&q=80" />
+                  <Box ml={2}>
+                    <Typography variant="subtitle2" component="p">
+                      Guy Clemons
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary" component="p">
+                      May 14, 2020
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box>
+                  <BookmarkBorderIcon />
+                </Box>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="https://images.pexels.com/photos/1181263/pexels-photo-1181263.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    React useContext
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                    across all continents except Antarctica
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions className={classes.cardActions}>
+                <Box className={classes.author}>
+                  <Avatar src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
+                  <Box ml={2}>
+                    <Typography variant="subtitle2" component="p">
+                      Guy Clemons
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary" component="p">
+                      May 14, 2020
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box>
+                  <BookmarkBorderIcon />
+                </Box>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="https://images.pexels.com/photos/325111/pexels-photo-325111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    React useContext
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                    across all continents except Antarctica
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions className={classes.cardActions}>
+                <Box className={classes.author}>
+                  <Avatar src="https://images.unsplash.com/photo-1584999734482-0361aecad844?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=80" />
+                  <Box ml={2}>
+                    <Typography variant="subtitle2" component="p">
+                      Guy Clemons
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary" component="p">
+                      May 14, 2020
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box>
+                  <BookmarkBorderIcon />
+                </Box>
+              </CardActions>
+            </Card>
           </Grid>
         </Grid>
-        <Typography color="text.secondary" variant="body2">
-        Daha önceki yazılarımda da kısaca bahsettiğim gibi, geçtiğimiz dönem içerisinde 7
-        doktora başvurusu yaptım, bunlardan 2 tanesinden ise kabul aldım...
-        </Typography>
-      </Box>      
-      <Box sx={{ m: 2 }}>      
-      <Button>Devamını OKu</Button>
-      </Box>      
-      <Divider variant="middle" />
-
-
-      <Box sx={{ mx:2,mt:4 }}>
-        <Grid container alignItems="center">
-          <Grid item xs>
-            <Typography gutterBottom variant="h5" component="div">
-            Staj Rehberi#2- Niyet Mektubu
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography gutterBottom variant="h6" component="div">
-            2 Haziran 2021
-            </Typography>
-          </Grid>
-        </Grid>
-        <Typography color="text.secondary" variant="body2">
-        Niyet mektubu, başvurunuz esnasında kendinizi insanlaştırmanızı, başvurudaki ve özgeçmişinizdeki
-        bilgileri belli bir bağlam içerisine yerleştirmenizi...
-        </Typography>
-      </Box>      
-      <Box sx={{ m: 2 }}>      
-      <Button>Devamını OKu</Button>
-      </Box>      
-      <Divider variant="middle" />
-
-      <Box sx={{ mx:2,mt:4 }}>
-        <Grid container alignItems="center">
-          <Grid item xs>
-            <Typography gutterBottom variant="h5" component="div">
-            Staj Rehberi#1- Özgeçmiş Hazırlama
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography gutterBottom variant="h6" component="div">
-            26 Mayıs 2021
-            </Typography>
-          </Grid>
-        </Grid>
-        <Typography color="text.secondary" variant="body2">
-        Hayal edin, hedefiniz olan şirkette bir insan kaynakları çalışanı olarak yer alıyorsunuz. Staj başvuru döneminin sonlarına doğru, her gün
-         önünüzden onlarca, belki de yüzlerce başvurunun geçtiği haftalar...
-        </Typography>
-      </Box>      
-      <Box sx={{ m: 2 }}>      
-      <Button>Devamını OKu</Button>
-      </Box>      
-      <Divider variant="middle" />
-
-      <Box sx={{ mx:2,mt:4 }}>
-        <Grid container alignItems="center">
-          <Grid item xs>
-            <Typography gutterBottom variant="h5" component="div">
-            Lisans Araştırmaları- Giriş Rehberi
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography gutterBottom variant="h6" component="div">
-            26 Mayıs 2021
-            </Typography>
-          </Grid>
-        </Grid>
-        <Typography color="text.secondary" variant="body2">
-        Bu yazıya küçük bir dipnotla başlamak iyi olacaktır diye düşünüyorum. Elbette
-        ki kendime bir araştırmacı diyecek niteliklere sahip değilim...
-        </Typography>
-      </Box>      
-      <Box sx={{ m: 2 }}>      
-      <Button>Devamını OKu</Button>
-      </Box>      
-      <Divider variant="middle" />
-
-      <Box sx={{ mx:2,mt:4 }}>
-        <Grid container alignItems="center">
-          <Grid item xs>
-            <Typography gutterBottom variant="h5" component="div">
-              Doktora Başvuruları: Kapsamlı Rehber
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography gutterBottom variant="h6" component="div">
-             16 Nisan 2021
-            </Typography>
-          </Grid>
-        </Grid>
-        <Typography color="text.secondary" variant="body2">
-        Daha önceki yazılarımda da kısaca bahsettiğim gibi, geçtiğimiz dönem içerisinde 7
-        doktora başvurusu yaptım, bunlardan 2 tanesinden ise kabul aldım...
-        </Typography>
-      </Box>      
-      <Box sx={{ m: 2 }}>      
-      <Button>Devamını OKu</Button>
-      </Box>      
-      <Divider variant="middle" />
-    </Box>
-    </>
-            
-            
-       )
+        <Box my={4} className={classes.paginationContainer}>
+          <Pagination count={10} />
+        </Box>
+      </Container>
+    </div>
+  );
 }
 
-export default Blog; 
+export default Blog;
