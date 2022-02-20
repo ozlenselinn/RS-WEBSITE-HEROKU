501ec2b819aa7b5e38af77f167f68661d939050d
Gizem Sarıçiçek <saricicek.gizem07@gmail.com>
2022-02-20 17:58:30 +0300

    değerler sayfası footer düzeltildi.

--- src/Components/Pages/Kurumsal/Degerler.js
+++ src/Components/Pages/Kurumsal/Degerler.js
@@ -1,16 +1,12 @@
 import React from "react";
-import MotivContentValues from "./MotivContentValues";
 import MotivContentMotiv from "./MotivContentMotiv";
 import "./DegerlerStyle.css";
 
 function Degerler(props) {
-
   return (
-    <div className='odinMainContainer'>
-      <div  className='odinLeftSidebarContainer'> <MotivContentValues>
-      </MotivContentValues></div>
-     <div className='odinMainContentContainer'> <MotivContentMotiv>
-      </MotivContentMotiv></div>
+  
+    <div>
+      <MotivContentMotiv></MotivContentMotiv>
     </div>
   );
 }
