8f727f5af5f6e7735e965e8762fed7efaeedaa53
sugarabia <knkrabia@hotmail.com>
2022-03-14 20:10:20 +0300

    home svg denendi

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import arkaplan from './arkaplan.png';
 // import arkaplan from './arkaplan.svg';
 import HomeStyle from './HomeStyle.css';
-import arkaplan from './home_back.jpeg';
+import arkaplan from './home_back.svg';
 
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous"></link>
 //import amblem from './amblem.png';
